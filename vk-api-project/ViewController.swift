d379587a206e969c02d120ce3cbb3126e9009cb0
nekitback <artamnikita@yandex.ru>
2022-09-08 15:18:01 +0300

    build: initialize project

---
+++ vk-api-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vk-api-project
+//
+//  Created by Nikita Artamonov on 08.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
