137b408de5ddedfa1213bc464faaa89eb1548dc3
nekitback <artamnikita@yandex.ru>
2022-09-10 11:00:49 +0300

    feat: add tabbar

---
+++ vk-api-project/Screens/Communities/CommunitiesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CommunitiesViewController.swift
+//  vk-api-project
+//
+//  Created by Nikita Artamonov on 10.09.2022.
+//
+
+import UIKit
+
+class CommunitiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemOrange
+    }
+    
+}
